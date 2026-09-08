0c44bc2a44465f5877d18340794e7075f4191d47
harshithaakkina <harshithaakkina11@email.com>
2026-09-08 20:10:10 +0530

    first

---
+++ variables.js
@@ -0,0 +1,27 @@
+//basically, variables are used to store values that we can use and modify in our programs.
+
+
+// let can reassign $ can not re-declare
+
+let name = "honey"
+ name = "john"
+
+console.log(name) //John
+
+let num = 11
+num = 12 
+console.log(num) //12
+
+
+//var can reassign & re-declare variables values
+
+var fruit = "mango"
+fruit = 'apple'
+var fruit = 'banana'
+
+console.log(fruit)//banana
+
+// const → Cannot be reassigned or redeclared
+
+const nums = 1+1
+console.log(nums)//2
